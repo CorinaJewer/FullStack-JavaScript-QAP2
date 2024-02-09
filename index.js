fabc99f65819471d3592f092a7d981c3b8277a48
Corina Jewer <corina.jewer@keyin.com>
2024-02-09 19:41:40 -0330

    Issue #2 Complete - Reading html files from views folder

--- index.js
+++ index.js
@@ -1,53 +1,72 @@
 const http = require('http');
+const fs = require ('fs');
 global.DEBUG = true;
 const port = 3000;
 
-const server = http.createServer((request, response)=>{
+const server = http.createServer((request, response) => {
     if (DEBUG)console.log('Requested URL:', request.url)
+    let path = "./views/";
 
     switch (request.url){
+
         case '/':
             if (DEBUG) console.log ('Home Page');
-            response.writeHead(200, 'Content-Type', 'text/html');
-            response.end('<p>Home page is working.</p>');
+            path += 'index.html';
+            if (DEBUG)console.log('Path:', path);
+            fetchFile(path);
             break;
 
         case '/about':
             if (DEBUG) console.log ('About Page');
-            response.writeHead(200, 'Content-Type', 'text/html');
-            response.end('<p>About page is working.</p>');
+            path += 'about.html';
+            if (DEBUG) console.log('Path:', path);
+            fetchFile(path);
             break;
 
         case '/subscribe':
             if (DEBUG) console.log ('Subscribe Page');
-            response.writeHead(200, 'Content-Type', 'text/html');
-            response.end('<p>Subscribe page is working.</p>');
+            path += 'subscribe.html';
+            if (DEBUG) console.log('Path:', path);
+            fetchFile(path);
             break;
 
         case '/products':
             if (DEBUG) console.log ('Products Page');
-            response.writeHead(200, 'Content-Type', 'text/html');
-            response.end('<p>Products page is working.</p>');
+            path += 'products.html';
+            if (DEBUG) console.log('Path:', path);
+            fetchFile(path);
             break;
 
         case '/contact':
             if (DEBUG) console.log ('Contact Page');
-            response.writeHead(200, 'Content-Type', 'text/html');
-            response.write('<p>Contact page is working.</p>');
-            response.end();
+            path += 'contact.html';
+            if (DEBUG) console.log('Path:', path);
+            fetchFile(path);
             break;
         
         default:
             if (DEBUG) console.log ('404 not found');
-            response.writeHead(404, 'Content-Type', 'text/html');
+            response.writeHead(404, {'Content-Type':'text/html'});
             response.end('404 not found');
             break;
-    }  
-})
+    };
+    
+    function fetchFile(filename){
+        fs.readFile(filename, (error,content)=>{
+            if (error){
+            response.writeHead(500, {'Content-Type':'text/plain'});
+            response.end('500 Internal Server Error');
+            }else{
+            response.writeHead(200, {'Content-Type':'text/html'});
+            response.end(content,'utf-8');
+            };
+        }) ; 
+    };
+});
 
 server.listen(port,() => {
     console.log(`Server is sucessfully running on port ${port}.`)
-})
+});
 
 
 
